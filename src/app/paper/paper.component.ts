b933fe982f6e7111da0889803b12821700019fcc
Théo <theo_38180@live.fr>
2021-02-15 21:03:20 +0100

    quelques ajouts pour la liste

--- src/app/paper/paper.component.ts
+++ src/app/paper/paper.component.ts
@@ -11,4 +11,24 @@ export class PaperComponent implements OnInit {
   ngOnInit(): void {}
 
   nom = 'Théo';
+  papiers = [
+    {
+      id: '12',
+      nom: 'Canson feuille trop bien',
+      grammage: '300g',
+      couleur: 'bleu',
+    },
+    {
+      id: '13',
+      nom: 'Non',
+      grammage: '100g',
+      couleur: 'jsppp',
+    },
+    {
+      id: '13',
+      nom: 'Non',
+      grammage: '8768452g',
+      couleur: 'aaaaaaaaaaaaaaa',
+    },
+  ];
 }
