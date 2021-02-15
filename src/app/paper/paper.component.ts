fd035ac754bc2a73a41c748e252eff0e0a9798df
Théo <theo_38180@live.fr>
2021-02-15 20:31:57 +0100

    înitial commit

---
+++ src/app/paper/paper.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paper',
+  templateUrl: './paper.component.html',
+  styleUrls: ['./paper.component.css'],
+})
+export class PaperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  nom = 'Théo';
+}
