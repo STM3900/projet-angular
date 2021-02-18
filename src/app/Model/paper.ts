7d0bec84d9cdd897da257f7bba5849aa4000f15a
Théo <theo_38180@live.fr>
2021-02-18 17:40:24 +0100

    partie 1 ok

--- src/app/Model/paper.ts
+++ src/app/Model/paper.ts
@@ -1,2 +1,7 @@
 export class Paper {
+  public id = 0;
+  public nom = '';
+  public texture = '';
+  public grammage = '';
+  public couleur = '';
 }
--- src/app/paper/paper.component.ts
+++ src/app/paper/paper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Paper } from '../Model/paper';
 
 @Component({
   selector: 'app-paper',
@@ -8,27 +9,60 @@ import { Component, OnInit } from '@angular/core';
 export class PaperComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
-
   nom = 'Théo';
-  papiers = [
+  tooAdd = false;
+  public papiers: Paper[] = [
     {
-      id: '12',
+      id: 1,
       nom: 'Canson feuille trop bien',
+      texture: 'oui',
       grammage: '300g',
       couleur: 'bleu',
     },
     {
-      id: '13',
+      id: 2,
       nom: 'Non',
+      texture: 'oui',
       grammage: '100g',
       couleur: 'jsppp',
     },
     {
-      id: '13',
+      id: 3,
       nom: 'Non',
+      texture: 'oui',
       grammage: '8768452g',
       couleur: 'aaaaaaaaaaaaaaa',
     },
   ];
+
+  ngOnInit(): void {}
+
+  public selectedPaper;
+
+  onSelect(papier: Paper) {
+    if (!this.selectedPaper) {
+      this.selectedPaper = papier;
+      this.tooAdd = false;
+    }
+  }
+
+  onDeselect() {
+    this.selectedPaper = null;
+  }
+
+  addPaper() {
+    let newPaper = new Paper();
+    newPaper.id = this.papiers.length + 1;
+    this.selectedPaper = newPaper;
+    this.tooAdd = true;
+  }
+
+  onValidate() {
+    if (this.tooAdd) {
+      this.papiers.push(this.selectedPaper);
+      this.selectedPaper = null;
+    } else {
+      //TODO tg
+    }
+  }
 }
